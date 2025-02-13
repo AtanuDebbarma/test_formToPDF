435b05778cc71fb05f1e6e3cd0486ec148c6b5e6
Atanu_Mobeet <atanu.fat.mobeet@gmail.com>
2025-02-13 21:38:24 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{ src: "index.html", dest: "" }],
-    }),
-  ],
-  base: "/test_formToPDF/", // Set base URL
+  plugins: [react()],
+  base: '/test_formToPDF/', // <-- Set this to your repo name
 });
