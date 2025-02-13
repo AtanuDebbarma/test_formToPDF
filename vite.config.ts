c0d8a66174cc285efe02aba6634c7096fdf7f2c1
Atanu_Mobeet <atanu.fat.mobeet@gmail.com>
2025-02-13 21:44:03 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/test_formToPDF/', // <-- Set this to your repo name
+  base: "/test_formToPDF/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
